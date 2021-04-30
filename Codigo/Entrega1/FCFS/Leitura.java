bcac09faecce5af0b0f7f334d726b003de9eb821
Mateus Samartini <66256202+Samartinho@users.noreply.github.com>
2021-04-30 20:08:31 -0300

    Create Leitura.java

---
+++ Codigo/Entrega1/FCFS/Leitura.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Leitura {
+	private static BufferedReader entrada;
+
+	public void abrirArquivo(String nomeArquivo) throws FileNotFoundException{	
+			entrada = new BufferedReader(new FileReader(nomeArquivo));
+	}
+	
+	public void fecharArquivo() throws IOException {
+			entrada.close();	
+	}
+	
+	public String ler() throws IOException {
+		String textoEntrada;
+		textoEntrada = entrada.readLine();
+		return textoEntrada;
+	}
+}
