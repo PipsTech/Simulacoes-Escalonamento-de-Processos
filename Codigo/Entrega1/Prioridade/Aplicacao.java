6a707b7fc4278d13e8967825e6b1d186f128b15e
Bruno Silveira Cequeira Lima <brunobhsclima@gmail.com>
2021-04-30 20:11:27 -0300

    Update and rename trabpratico.java to Aplicacao.java

--- Codigo/Entrega1/Prioridade/Aplicacao.java
+++ Codigo/Entrega1/Prioridade/Aplicacao.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class trabpratico{
+public class Aplicacao{
 
     public static void main(String[]args) throws IOException{
         Prioridade p = new Prioridade();
